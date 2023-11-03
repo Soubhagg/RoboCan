0a7af2b2fc6c4a834fb870fbac3cb4473b326bde
MiguelAHasegawa <145897722+MiguelAHasegawa@users.noreply.github.com>
2023-11-03 15:48:59 +1100

    Nut123

--- Assignment2/Assignment2.m
+++ Assignment2/Assignment2.m
@@ -572,6 +572,186 @@ classdef Assignment2 < handle
                 disp(['Sugars: ' sugars{1}]);
                 disp(['Sodium: ' sodium{1}]);
             end
+                        function nut2_cb(~,~)
+
+                canInfo = imread("131559_2.jpg");
+
+                try
+                    imageInfo = ocr(canInfo);
+                    recognizedText = imageInfo.Text;
+                catch
+                    recognizedText = 'Text not recognized';
+                end
+                
+                % Display the image and recognized text
+                figure
+                imshow(canInfo)
+                
+                % Initialize variables to store nutritional information
+                servSize = '';
+                energy = '';
+                protein = '';
+                fat = '';
+                carbs = '';
+                sugars = '';
+                sodium = '';
+                
+                % Define regular expressions to capture nutritional information
+                servSizePattern = 'Serving size:\s*([\d.]+\s*[a-zA-Z]+)';
+                energyPattern = 'Energy\s*([\d.]+[a-zA-Z]+)\s*\((\d+\s*Cal)\)';
+                proteinPattern = 'Protein[^0-9]*([\d.]+[a-zA-Z]*)';
+                fatPattern = 'Fat[^0-9]*([\d.]+[a-zA-Z]*)';
+                carbsPattern = 'Carbohydrate[^0-9]*([\d.]+)\s*([a-zA-Z]*)';
+                sugarsPattern = 'Sugars[^0-9]*([\d.]+[a-zA-Z]+)';
+                sodiumPattern = 'Sodium\s*([\d.]+)\s*(mg)';
+                
+                % Match the regular expressions and extract the data
+                servSizeMatch = regexp(recognizedText, servSizePattern, 'tokens');
+                energyMatch = regexp(recognizedText, energyPattern, 'tokens');
+                proteinMatch = regexp(recognizedText, proteinPattern, 'tokens');
+                fatMatch = regexp(recognizedText, fatPattern, 'tokens');
+                carbsMatch = regexp(recognizedText, carbsPattern, 'tokens');
+                sugarsMatch = regexp(recognizedText, sugarsPattern, 'tokens');
+                sodiumMatch = regexp(recognizedText, sodiumPattern, 'tokens');
+                
+                % Extract the matched values
+                if ~isempty(servSizeMatch)
+                    servSize = servSizeMatch{1};
+                end
+                
+                if ~isempty(energyMatch)
+                    energy = energyMatch{1};
+                end
+                
+                if ~isempty(proteinMatch)
+                    protein = proteinMatch{1};
+                end
+                
+                if ~isempty(fatMatch)
+                    fat = fatMatch{1};
+                end
+                
+                if ~isempty(carbsMatch)
+                    carbsValue = carbsMatch{1}{1};
+                    carbsUnit = carbsMatch{1}{2};
+                    % If the unit is empty, set it to "g" (grams)
+                    if isempty(carbsUnit)
+                        carbsUnit = 'g';
+                    end
+                    % Append the unit to the value
+                    carbs = carbsValue + " " + carbsUnit;
+                else
+                    % If no carbohydrate information is found, set it to "Not available"
+                    carbs = 'Not available';
+                end
+                
+                if ~isempty(sugarsMatch)
+                    sugars = sugarsMatch{1};
+                end
+                
+                if ~isempty(sodiumMatch)
+                    sodium = sodiumMatch{1};
+                end
+                
+                % Display the extracted information
+                disp(['Serving Size: ' servSize{1}]);
+                disp(['Energy: ' energy{1} ' ' energy{2}]);
+                disp(['Protein: ' protein{1}]);
+                disp(['Fat: ' fat{1}]);
+                disp(['Carbohydrates: ' carbs{1}]);
+                disp(['Sugars: ' sugars{1}]);
+                disp(['Sodium: ' sodium{1}]);
+                        end
+            function nut3_cb(~,~)
+
+                canInfo = imread("131559_2.jpg");
+
+                try
+                    imageInfo = ocr(canInfo);
+                    recognizedText = imageInfo.Text;
+                catch
+                    recognizedText = 'Text not recognized';
+                end
+                
+                % Display the image and recognized text
+                figure
+                imshow(canInfo)
+                
+                % Initialize variables to store nutritional information
+                servSize = '';
+                energy = '';
+                protein = '';
+                fat = '';
+                carbs = '';
+                sugars = '';
+                sodium = '';
+                
+                % Define regular expressions to capture nutritional information
+                servSizePattern = 'Serving size:\s*([\d.]+\s*[a-zA-Z]+)';
+                energyPattern = 'Energy\s*([\d.]+[a-zA-Z]+)\s*\((\d+\s*Cal)\)';
+                proteinPattern = 'Protein[^0-9]*([\d.]+[a-zA-Z]*)';
+                fatPattern = 'Fat[^0-9]*([\d.]+[a-zA-Z]*)';
+                carbsPattern = 'Carbohydrate[^0-9]*([\d.]+)\s*([a-zA-Z]*)';
+                sugarsPattern = 'Sugars[^0-9]*([\d.]+[a-zA-Z]+)';
+                sodiumPattern = 'Sodium\s*([\d.]+)\s*(mg)';
+                
+                % Match the regular expressions and extract the data
+                servSizeMatch = regexp(recognizedText, servSizePattern, 'tokens');
+                energyMatch = regexp(recognizedText, energyPattern, 'tokens');
+                proteinMatch = regexp(recognizedText, proteinPattern, 'tokens');
+                fatMatch = regexp(recognizedText, fatPattern, 'tokens');
+                carbsMatch = regexp(recognizedText, carbsPattern, 'tokens');
+                sugarsMatch = regexp(recognizedText, sugarsPattern, 'tokens');
+                sodiumMatch = regexp(recognizedText, sodiumPattern, 'tokens');
+                
+                % Extract the matched values
+                if ~isempty(servSizeMatch)
+                    servSize = servSizeMatch{1};
+                end
+                
+                if ~isempty(energyMatch)
+                    energy = energyMatch{1};
+                end
+                
+                if ~isempty(proteinMatch)
+                    protein = proteinMatch{1};
+                end
+                
+                if ~isempty(fatMatch)
+                    fat = fatMatch{1};
+                end
+                
+                if ~isempty(carbsMatch)
+                    carbsValue = carbsMatch{1}{1};
+                    carbsUnit = carbsMatch{1}{2};
+                    % If the unit is empty, set it to "g" (grams)
+                    if isempty(carbsUnit)
+                        carbsUnit = 'g';
+                    end
+                    % Append the unit to the value
+                    carbs = carbsValue + " " + carbsUnit;
+                else
+                    % If no carbohydrate information is found, set it to "Not available"
+                    carbs = 'Not available';
+                end
+                
+                if ~isempty(sugarsMatch)
+                    sugars = sugarsMatch{1};
+                end
+                
+                if ~isempty(sodiumMatch)
+                    sodium = sodiumMatch{1};
+                end
+                
+                % Display the extracted information
+                disp(['Serving Size: ' servSize{1}]);
+                disp(['Energy: ' energy{1} ' ' energy{2}]);
+                disp(['Protein: ' protein{1}]);
+                disp(['Fat: ' fat{1}]);
+                disp(['Carbohydrates: ' carbs{1}]);
+                disp(['Sugars: ' sugars{1}]);
+                disp(['Sodium: ' sodium{1}]);
+            end
 
             % Stop function sends signal to the system if being pushed
             function stop_cb(~,~)
